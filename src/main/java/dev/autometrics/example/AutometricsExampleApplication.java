a0d15c8acb5e97065c077714833b571c51a8a70b
James Daly <jamsiedaly@gmail.com>
2023-12-24 16:49:51 +0000

    Initial setup

---
+++ src/main/java/dev/autometrics/example/AutometricsExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.autometrics.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutometricsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutometricsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/autometrics/example/AutometricsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.autometrics.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutometricsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
